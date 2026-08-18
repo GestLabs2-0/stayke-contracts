0afd3d8d44d07ca97629b35526b8cb0a62df9873
gabotdev <gabrieltorres2014@gmail.com>
2026-08-18 09:11:36 -0400

    feat: add more validations for PDAs

--- programs/stayke-disputes/src/events.rs
+++ programs/stayke-disputes/src/events.rs
@@ -23,7 +23,6 @@ pub struct DisputeResolved {
 #[event]
 pub struct UserPenalized {
     pub penalized_user: Pubkey,
-    pub affected_wallet: Pubkey,
     pub penalty_amount: u64,
     pub timestamp: i64,
 }
